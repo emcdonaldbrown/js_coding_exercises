b4e277af66ee3fda6be5ab6c3becc9789d6b3759
Emma McDonald-Brown <e.mcdonald-brown@outlook.com>
2022-12-08 00:02:23 +0000

    One challenge still left in this exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,40 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+    
+    return sandwich.fillings;
+
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  
+    if (person.city === 'Manchester')
+      return true;
+    else
+      return false;
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+    return Math.ceil(people / 40);
+  
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  
+    var count = 0;
+    for(var i = 0; i < arr.length; ++i){
+        if(arr[i] == 'sheep')
+            count++;
+    }
+
+    return count;
+      
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
 }
