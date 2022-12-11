aa845b94f7533d5ad2f99f4ec793edd0608f96a2
Emma McDonald-Brown <e.mcdonald-brown@outlook.com>
2022-12-11 23:30:16 +0000

    menu items

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,6 +1,10 @@
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+
+
+  const squared = nums.map(num => (num * num));
+
+  return squared;
 }
 
 export function camelCaseWords(words) {
@@ -16,17 +20,54 @@ export function camelCaseWords(words) {
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
-}
+
+    var iTotal = 0;
+
+    people.forEach((obj, i) => {
+      
+      iTotal = iTotal + (obj.subjects.length);
+    
+    });
+
+    return iTotal;
+  
+  }
 
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+
+    const filterValue = ingredient;
+    const filteredMenu = menu.filter(val => val.ingredients.includes(filterValue));
+
+  
+    console.log(filteredMenu);
+
+    if (filteredMenu === '[]')
+      return false;
+    else
+      return true;
+    
 }
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
   // Your code here!
+
+  var iAvail = false;
+
+  menu.forEach((obj, i) => {
+
+    console.log(obj.ingredients);
+    console.log(ingredient);
+
+    if ((obj.ingredients).includes(ingredient)) 
+        iAvail = true;
+      else
+        iAvail = false;
+  
+  });
+
+  return iAvail;
 }
